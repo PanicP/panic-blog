52ac06174b881efce609dd9adae31bd98f0a12e0
PanicP <filmous2136@gmail.com>
2022-09-01 01:08:32 +0700

    WIP: Added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
+import { Outlet, ReactLocation, Router } from '@tanstack/react-location'
 import LayoutHeader from 'components/layouts/Header/Header'
 import React from 'react'
 
 function App() {
-    return <LayoutHeader />
+    const location = new ReactLocation()
+    return (
+        // <div>
+        //     <LayoutHeader />
+        //     <div>test</div>
+        // </div>
+        <Router
+            location={location}
+            routes={[
+                { path: '/', element: <div /> }, // home
+                {
+                    path: 'blogs',
+                    children: [
+                        { path: '/', element: <div>blog</div> }, // blogs
+                        {
+                            path: ':name',
+                            element: <div>param</div>,
+                        },
+                    ],
+                },
+            ]}
+        >
+            <LayoutHeader />
+            <Outlet /> {/* Start rendering router matches */}
+        </Router>
+    )
 }
 
 export default App
